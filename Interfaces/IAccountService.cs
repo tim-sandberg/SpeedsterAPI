e435f82b7b74f5052d3a6ccacc5d1f311879233c
Timothy Sandberg <tim@codegauchos.com>
2021-01-17 07:32:45 -0800

    Add project files.

---
+++ Interfaces/IAccountService.cs
@@ -0,0 +1,20 @@
+﻿using SpeedsterAPI.Models;
+using SpeedsterAPI.Models.Account;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SpeedsterAPI.Interfaces
+{
+    public interface IAccountService
+    {
+        User User { get; }
+        Task Initialize();
+        Task Login(Login model);
+        Task Logout();
+        Task Register(AddUser model);
+        Task<IList<User>> GetAll();
+        Task<User> GetById(string id);
+        Task Update(string id, EditUser model);
+        Task Delete(string id);
+    }
+}
